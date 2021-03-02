e687176a4436c68e68f81fde2664a4a0913c45ea
mobiden <mobiden@users.noreply.github.com>
2021-03-02 15:11:15 +0300

    add logfile change place of songer's names, add capitalize method for music files

--- main.py
+++ main.py
@@ -4,40 +4,15 @@ from tkinter import filedialog as fd
 
 
 
-def change_name_place():
-# меняет местами исполнителя и название
-    print("Текущая директория:", os.getcwd())
-    cur_dir = str(input())
-    #   cur_dir = fd.askopenfilename()
-    if cur_dir == '':
-        cur_dir = os.getcwd()
-    os.chdir(cur_dir)
-    all_files = os.listdir(cur_dir)
-    logfile = open('logfile.txt', 'w', encoding='utf-8')
-    for th_file in all_files:
-        if checkfilemp3(th_file):
-            cur_name = th_file
-            if th_file.count(' - ') > 0:
-                th_file = th_file[0: -4]
-                chnamelist = th_file.split(' - ')
-                th_file = chnamelist[1] + ' - ' + chnamelist[0] + '.mp3'
-
-            if cur_name != th_file:
-                print(f' изменяем: {cur_name} на {th_file}')
-
-                try:
-                    os.rename(cur_name, th_file)
-                    print(f'{cur_name} ; {th_file}', file=logfile)
-                except:
-                    print('Невозможно поменять файл', cur_name)
-    print('done')
-    logfile.close()
 
 
-
-
-answ = int(input("Удалять цифры спереди - 1, Переставить местами - 2: "))
+answ = int(input("Удалять цифры спереди - 1:, "
+                 "Переставить местами - 2:, "
+                 "Все буквы, кроме первой строчные - 3:. Номер? "))
 if answ == 1:
     del_fig()
 elif answ == 2:
     change_name_place()
+elif answ == 3:
+    capitalize_name()
+
--- util.py
+++ util.py
@@ -5,7 +5,7 @@ def del_fig():
     symb = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '+', '.', ' ']
 
     print("Текущая директория:", os.getcwd())
-    cur_dir = str(input())
+    cur_dir = str(input("Введите новую директорию: "))
 #   cur_dir = fd.askopenfilename()
     if cur_dir == '':
         cur_dir = os.getcwd()
@@ -38,3 +38,65 @@ def checkfilemp3 (checkfile):
         return True
     else:
         return False
+
+
+def change_name_place():
+# меняет местами исполнителя и название
+    print("Текущая директория:", os.getcwd())
+    cur_dir = str(input("Введите новую директорию: "))
+    #   cur_dir = fd.askopenfilename()
+    if cur_dir == '':
+        cur_dir = os.getcwd()
+    os.chdir(cur_dir)
+    all_files = os.listdir(cur_dir)
+    logfile = open('logfile.txt', 'w', encoding='utf-8')
+    for th_file in all_files:
+        if checkfilemp3(th_file):
+            cur_name = th_file
+            if th_file.count(' - ') > 0:
+                th_file = th_file[0: -4]
+                chnamelist = th_file.split(' - ')
+                th_file = chnamelist[1] + ' - ' + chnamelist[0] + '.mp3'
+
+            if cur_name != th_file:
+                print(f' изменяем: {cur_name} на {th_file}')
+
+                try:
+                    os.rename(cur_name, th_file)
+                    print(f'{cur_name} ; {th_file}', file=logfile)
+                except:
+                    print('Невозможно поменять файл', cur_name)
+    print('done')
+    logfile.close()
+
+def capitalize_name():
+# первые буквы большие - остальные маленькие
+    print("Текущая директория:", os.getcwd())
+    cur_dir = str(input("Введите новую директорию: "))
+    #   cur_dir = fd.askopenfilename()
+    if cur_dir == '':
+        cur_dir = os.getcwd()
+    os.chdir(cur_dir)
+    all_files = os.listdir(cur_dir)
+    logfile = open('logfile.txt', 'w', encoding='utf-8')
+    for th_file in all_files:
+        if checkfilemp3(th_file):
+            cur_name = th_file
+            if th_file.count('-') > 0:
+                chnamelist = th_file.split('-')
+                for i in range(len(chnamelist)):
+                    chnamelist[i] = chnamelist[i].strip().capitalize()
+                th_file = ' - '.join(chnamelist)
+            else:
+                th_file = th_file.capitalize()
+
+            if cur_name != th_file:
+                print(f' изменяем: {cur_name} на {th_file}')
+
+                try:
+                    os.rename(cur_name, th_file)
+                    print(f'{cur_name} ; {th_file}', file=logfile)
+                except:
+                    print('Невозможно поменять файл', cur_name)
+    print('done')
+    logfile.close()
